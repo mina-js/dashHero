4f23ae2701071e0f7baa20956d183e6d0adc379c
mina-js <aminatajsow@gmail.com>
2023-04-20 16:22:59 -0400

    progress with snapping to direction:

--- Assets/Scripts/RagdollController.cs
+++ Assets/Scripts/RagdollController.cs
@@ -2,26 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Currently just using this to pass variables to all the body part children
 public class RagdollController : MonoBehaviour
 {
   public float speed = 50f;
   public Camera mainCamera;
 
-  void Start()
-  {
-    EventManager.OnEventEmitted += HandleEvent;
-  }
+  // void Start()
+  // {
+  //   EventManager.OnEventEmitted += HandleEvent;
+  // }
 
-  void HandleEvent(string eventKey, object data)
-  {
-    Dictionary<string, object> dataDict = data as Dictionary<string, object>;
+  // void HandleEvent(string eventKey, object data)
+  // {
+  //   Dictionary<string, object> dataDict = data as Dictionary<string, object>;
 
-    if (dataDict == null)
-    {
-      Debug.Log("data is not a dictionary");
-      return;
-    }
+  //   if (dataDict == null)
+  //   {
+  //     Debug.Log("data is not a dictionary");
+  //     return;
+  //   }
 
-    Debug.Log("ragdoll knows about collision: " + dataDict["bodyPartName"] + " collided with " + dataDict["other"]);
-  }
+  //   Debug.Log("ragdoll knows about collision: " + dataDict["bodyPartName"] + " collided with " + dataDict["other"]);
+  // }
 }
